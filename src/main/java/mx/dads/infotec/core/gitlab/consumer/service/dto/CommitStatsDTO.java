8c4c52d7536ae240f9cec793277ad8666eaaea6e
Erik Valdivieso <erik.valdivieso@gmail.com>
2018-05-18 13:04:31 -0500

    Add CommitDTO

---
+++ src/main/java/mx/dads/infotec/core/gitlab/consumer/service/dto/CommitStatsDTO.java
@@ -0,0 +1,65 @@
+package mx.dads.infotec.core.gitlab.consumer.service.dto;
+
+import java.io.Serializable;
+import java.util.HashMap;
+import java.util.Map;
+import com.fasterxml.jackson.annotation.JsonAnyGetter;
+import com.fasterxml.jackson.annotation.JsonAnySetter;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class CommitStatsDTO implements Serializable {
+
+    @JsonProperty("additions")
+    private Integer additions;
+    @JsonProperty("deletions")
+    private Integer deletions;
+    @JsonProperty("total")
+    private Integer total;
+    @JsonIgnore
+    private Map<String, Object> additionalProperties = new HashMap<String, Object>();
+    private final static long serialVersionUID = -27527697034091943L;
+
+    @JsonProperty("additions")
+    public Integer getAdditions() {
+        return additions;
+    }
+
+    @JsonProperty("additions")
+    public void setAdditions(Integer additions) {
+        this.additions = additions;
+    }
+
+    @JsonProperty("deletions")
+    public Integer getDeletions() {
+        return deletions;
+    }
+
+    @JsonProperty("deletions")
+    public void setDeletions(Integer deletions) {
+        this.deletions = deletions;
+    }
+
+    @JsonProperty("total")
+    public Integer getTotal() {
+        return total;
+    }
+
+    @JsonProperty("total")
+    public void setTotal(Integer total) {
+        this.total = total;
+    }
+
+    @JsonAnyGetter
+    public Map<String, Object> getAdditionalProperties() {
+        return this.additionalProperties;
+    }
+
+    @JsonAnySetter
+    public void setAdditionalProperty(String name, Object value) {
+        this.additionalProperties.put(name, value);
+    }
+
+}
